72fb825233afe4bba7f205fc2af37b2d71eff127
laflosh <l_florent_l@hotmail.fr>
2024-08-28 16:33:24 +0200

    implémentation méthode main

---
+++ src/com/openclassrooms/cardgame/games/Games.java
@@ -0,0 +1,17 @@
+package com.openclassrooms.cardgame.games;
+
+import com.openclassrooms.cardgame.controller.GameController;
+import com.openclassrooms.cardgame.model.Deck;
+import com.openclassrooms.cardgame.view.View;
+
+public class Games {
+
+	public static void main(String[] args) {
+		
+		GameController gc = new GameController(new Deck(), new View());
+		
+		gc.run();
+		
+	}
+	
+}
